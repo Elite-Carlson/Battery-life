325a2d6f69436da80c3dc4deb6d767da49b77845
Elite Carlson <elitecarlson@gmail.com>
2022-09-24 06:57:20 +0200

    not done resume in the future, stable push

--- background.js
+++ background.js
@@ -20,88 +20,40 @@ function timeupdate() {
 setInterval(timeupdate, 1000);
 
 //Extension infographics
-navigator.getBattery().then((battery) => {
-    function updateLevelInfo2(){
-        if (Math.round(battery.level * 100) == 19) {
-            notify();//Notifying
-        }
-        percentage.innerHTML = Math.round(battery.level * 100);
-        progress.style.background = `conic-gradient(rgb(255, 255, 255) ${Math.round(battery.level * 100) * 3.6}deg,rgba(255, 255, 255, 0.137) 0deg)`;
-    }
-    function updateChargeInfo2(){
-        if (battery.charging == true) {
-            batterystatus.innerHTML = "Charging..";
-        } else {
-            batterystatus.innerHTML = "Unplugged";
-        }
-    }
-    updateLevelInfo2();
-    updateChargeInfo2();
-    battery.addEventListener('levelchange', updateLevelInfo2);
-    battery.addEventListener('chargingchange', updateChargeInfo2);
+// navigator.getBattery().then((battery) => {
+//     function updateLevelInfo2(){
+//         if (Math.round(battery.level * 100) == 95) {
+//             notify();//Notifying
+//         }
+//         percentage.innerHTML = Math.round(battery.level * 100);
+//         progress.style.background = `conic-gradient(rgb(255, 255, 255) ${Math.round(battery.level * 100) * 3.6}deg,rgba(255, 255, 255, 0.137) 0deg)`;
+//     }
+//     function updateChargeInfo2(){
+//         if (battery.charging == true) {
+//             batterystatus.innerHTML = "Charging..";
+//         } else {
+//             batterystatus.innerHTML = "Unplugged";
+//         }
+//     }
+//     updateLevelInfo2();
+//     updateChargeInfo2();
+//     battery.addEventListener('levelchange', updateLevelInfo2);
+//     battery.addEventListener('chargingchange', updateChargeInfo2);
     
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        var iconimg = 'light.png';
-    }else{
-        var iconimg = 'dark.png';
-    }
+//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//         var iconimg = 'light.png';
+//     }else{
+//         var iconimg = 'dark.png';
+//     }
 
-    function notify(){
-        chrome.notifications.create(
-            {
-                title: "Battery life",
-                message: `It's time to unplag buddy your at ` + Math.round(battery.level) * 100 +'%',
-                iconUrl: iconimg,
-                type: "basic"
-            }
-        )
-    }
-});
-
-//Push Notifications
-// function notifier(){
-//         Notification.requestPermission().then(permission => {
-//         notify();
-//         });
-// }
-// function notify(){    
-//     navigator.getBattery().then((battery) => {
-//         function updateAllBatteryInfo(){
-//             updateChargeInfo();
-//             updateLevelInfo();
-//         }
-//         updateAllBatteryInfo();
-//         function updateChargeInfo(){
-//             // console.log(`Battery charging? ${battery.charging ? "Yes" : "No"}`);
-//         }
-//         function updateLevelInfo(){
-//             if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//                 var iconimg = 'charginglight.png';
-//             }else{
-//                 var iconimg = 'chargingdark.png';
+//     function notify(){
+//         chrome.notifications.create(
+//             {
+//                 title: "Battery life",
+//                 message: `It's time to unplag buddy your at ` + Math.round(battery.level) * 100 +'%',
+//                 iconUrl: iconimg,
+//                 type: "basic"
 //             }
-//             if (!("Notification" in window)) {
-//                 alert("This browser does not support desktop notification");
-//             } else if (Notification.permission === "granted") {
-//               new Notification('Battery Life',{
-//                 body: `It's time to unplag buddy your at ` + battery.level * 100 +'%',
-//                 icon: iconimg,
-//                 requireInteraction: true
-//             });
-//             } else if (Notification.permission !== "denied") {
-//                 Notification.requestPermission().then((permission) => {
-//                     if (permission === "granted") {
-//                       new Notification('Time to Unplag',{
-//                         body: 'Battery level:' + battery.level * 100,
-//                         icon: iconimg,
-//                         requireInteraction: true
-//                     });
-//                 }
-//             });
-//             }
-//             // console.log(`Battery level: ${battery.level * 100}%`);
-//         }
-        // battery.addEventListener('levelchange', updateLevelInfo);
-        // battery.addEventListener('chargingchange', updateChargeInfo);
-//     });
-// }
+//         )
+//     }
+// });
