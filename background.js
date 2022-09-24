bdef766278f82fa3c04399c2209646373f00c0d2
Elite Carlson <elitecarlson@gmail.com>
2022-09-24 06:59:09 +0200

    Still Buggy

--- background.js
+++ background.js
@@ -19,41 +19,41 @@ function timeupdate() {
 }   
 setInterval(timeupdate, 1000);
 
-//Extension infographics
-// navigator.getBattery().then((battery) => {
-//     function updateLevelInfo2(){
-//         if (Math.round(battery.level * 100) == 95) {
-//             notify();//Notifying
-//         }
-//         percentage.innerHTML = Math.round(battery.level * 100);
-//         progress.style.background = `conic-gradient(rgb(255, 255, 255) ${Math.round(battery.level * 100) * 3.6}deg,rgba(255, 255, 255, 0.137) 0deg)`;
-//     }
-//     function updateChargeInfo2(){
-//         if (battery.charging == true) {
-//             batterystatus.innerHTML = "Charging..";
-//         } else {
-//             batterystatus.innerHTML = "Unplugged";
-//         }
-//     }
-//     updateLevelInfo2();
-//     updateChargeInfo2();
-//     battery.addEventListener('levelchange', updateLevelInfo2);
-//     battery.addEventListener('chargingchange', updateChargeInfo2);
+// Extension infographics
+navigator.getBattery().then((battery) => {
+    function updateLevelInfo2(){
+        if (Math.round(battery.level * 100) == 95) {
+            notify();//Notifying
+        }
+        percentage.innerHTML = Math.round(battery.level * 100);
+        progress.style.background = `conic-gradient(rgb(255, 255, 255) ${Math.round(battery.level * 100) * 3.6}deg,rgba(255, 255, 255, 0.137) 0deg)`;
+    }
+    function updateChargeInfo2(){
+        if (battery.charging == true) {
+            batterystatus.innerHTML = "Charging..";
+        } else {
+            batterystatus.innerHTML = "Unplugged";
+        }
+    }
+    updateLevelInfo2();
+    updateChargeInfo2();
+    battery.addEventListener('levelchange', updateLevelInfo2);
+    battery.addEventListener('chargingchange', updateChargeInfo2);
     
-//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//         var iconimg = 'light.png';
-//     }else{
-//         var iconimg = 'dark.png';
-//     }
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        var iconimg = 'light.png';
+    }else{
+        var iconimg = 'dark.png';
+    }
 
-//     function notify(){
-//         chrome.notifications.create(
-//             {
-//                 title: "Battery life",
-//                 message: `It's time to unplag buddy your at ` + Math.round(battery.level) * 100 +'%',
-//                 iconUrl: iconimg,
-//                 type: "basic"
-//             }
-//         )
-//     }
-// });
+    function notify(){
+        chrome.notifications.create(
+            {
+                title: "Battery life",
+                message: `It's time to unplag buddy your at ` + Math.round(battery.level) * 100 +'%',
+                iconUrl: iconimg,
+                type: "basic"
+            }
+        )
+    }
+});
